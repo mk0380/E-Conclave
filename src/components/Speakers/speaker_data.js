0907f73a050ca86c44fc2e85e489ee335d7a35a1
Mayank <mayankkumar03808766@gmail.com>
2023-02-11 15:30:53 +0530

    data updated

--- src/components/Speakers/speaker_data.js
+++ src/components/Speakers/speaker_data.js
@@ -1,10 +1,16 @@
-import Logo from '../img/card2.png'
+import Sachin from './sachin.jpg'
+import Harsh from './harsh.jpg'
+import Murthy from './murthy.jpg'
+import Naresh from './naresh.jpg'
+import Rajat from './rajat.jpg'
+import Swati from './swati.jpg'
 
-const speaker = [{ img: Logo, name: "Amol Kodag", work: "ENGINEERING LEADER" },
-{ img: Logo, name: "Amol Kodag", work: "ENGINEERING LEADER" }, { img: Logo, name: "Amol Kodag", work: "ENGINEERING LEADER" },
-{ img: Logo, name: "Amol Kodag", work: "ENGINEERING LEADER" }, { img: Logo, name: "Amol Kodag", work: "ENGINEERING LEADER" },
-{ img: Logo, name: "Amol Kodag", work: "ENGINEERING LEADER" }, { img: Logo, name: "Amol Kodag", work: "ENGINEERING LEADER" },
-{ img: Logo, name: "Amol Kodag", work: "ENGINEERING LEADER" }, { img: Logo, name: "Amol Kodag", work: "ENGINEERING LEADER" },
-{ img: Logo, name: "Amol Kodag", work: "ENGINEERING LEADER" }, { img: Logo, name: "Amol Kodag", work: "ENGINEERING LEADER" },]
+const speaker = [
+{ img: Sachin, name: "Sachin Bansal", work: "Founder, Flipkart" },
+{ img: Rajat, name: "Rajat Sharma", work: "Chairman and Editor-in-Chief, India TV Padma Bhushan Recipient" },
+{ img: Murthy, name: "Mahesh Murthy", work: "Founder, Pinstorm and Seedfund" },
+{ img: Naresh, name: "Naresh Gupta", work: "MD & CEO, Adobe India" },
+{ img: Swati, name: "Swati Murarka", work: "VP, Inventus India" },
+{ img: Harsh, name: "Harsh Manglik", work: "Former Chairman, Accenture India" },]
 
 export default speaker;
